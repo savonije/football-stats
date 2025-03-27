e1b5fc358441928ad348251134ea68dae2447412
Kevin Savonije <kevin@23g.nl>
2025-03-27 20:20:08 +0100

    feat: loader component

--- eslint.config.ts
+++ eslint.config.ts
@@ -9,18 +9,24 @@ import skipFormatting from '@vue/eslint-config-prettier/skip-formatting'
 // More info at https://github.com/vuejs/eslint-config-typescript/#advanced-setup
 
 export default defineConfigWithVueTs(
-  {
-    name: 'app/files-to-lint',
-    files: ['**/*.{ts,mts,tsx,vue}'],
-  },
+    {
+        name: 'app/files-to-lint',
+        files: ['**/*.{ts,mts,tsx,vue}'],
+    },
 
-  {
-    name: 'app/files-to-ignore',
-    ignores: ['**/dist/**', '**/dist-ssr/**', '**/coverage/**'],
-  },
+    {
+        name: 'app/files-to-ignore',
+        ignores: ['**/dist/**', '**/dist-ssr/**', '**/coverage/**'],
+    },
 
-  pluginVue.configs['flat/essential'],
-  vueTsConfigs.recommended,
-  ...oxlint.configs['flat/recommended'],
-  skipFormatting,
+    pluginVue.configs['flat/essential'],
+    vueTsConfigs.recommended,
+    ...oxlint.configs['flat/recommended'],
+    skipFormatting,
+
+    {
+        rules: {
+        'vue/multi-word-component-names': 'off',
+        },
+    }
 )
