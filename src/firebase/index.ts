b30ce79480d88a3745629da71e35743ec112ee59
Kevin Savonije <kevin@23g.nl>
2025-03-27 16:31:40 +0100

    fix: firebase

--- src/firebase/index.ts
+++ src/firebase/index.ts
@@ -4,7 +4,6 @@ import { getAnalytics } from "firebase/analytics";
 const firebaseConfig = {
     apiKey: import.meta.env.VITE_FIREBASE_APIKEY,
     authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
-    databaseURL: import.meta.env.VITE_FIREBASE_DB_URL,
     projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
     storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
     messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGE_SENDER_ID,
