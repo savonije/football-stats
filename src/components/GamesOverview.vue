a1cb05af824defb5e08222a7bd955a18044669af
Kevin Savonije <kevin@23g.nl>
2025-03-28 15:28:01 +0100

    feat: link to a detail page

--- src/components/GamesOverview.vue
+++ src/components/GamesOverview.vue
@@ -17,21 +17,21 @@
 <template>
     <div class="mb-9 flex items-center justify-between gap-3">
         <h1 class="mb-0">Wedstrijden</h1>
-
         <AddGame />
     </div>
 
     <div class="flex flex-col gap-4">
-        <div
+        <router-link
             v-for="game in Games"
             :key="game.id"
-            class="bg-shark-50 flex items-center justify-center gap-3 rounded-md p-4 shadow-xs"
+            class="bg-shark-50 hover:bg-shark-100 flex cursor-pointer items-center justify-center gap-3 rounded-md p-4 shadow-xs"
+            :to="{ name: 'detail', params: { id: game.id } }"
         >
             <div>{{ game.opponent }}</div>
             <div class="bg-shark-200 rounded-sm p-2 font-bold">
                 {{ game.goals_for }} - {{ game.goals_against }}
             </div>
             <div>Apollo</div>
-        </div>
+        </router-link>
     </div>
 </template>
