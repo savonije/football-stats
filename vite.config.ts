0497371abb145b104fd6ed2c5fe76b5d1ae36ca7
Kevin Savonije <kevin@23g.nl>
2025-09-06 19:05:19 +0200

    fix: actually load styling files

--- vite.config.ts
+++ vite.config.ts
@@ -1,20 +1,18 @@
 import { fileURLToPath, URL } from 'node:url'
 
+import eslint from '@nabla/vite-plugin-eslint'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
 import vueDevTools from 'vite-plugin-vue-devtools'
+import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-    vueJsx(),
-    vueDevTools(),
-  ],
+  plugins: [vue(), vueJsx(), vueDevTools(), tailwindcss(), eslint()],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
   },
 })
