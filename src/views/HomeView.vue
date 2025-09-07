d01a48d51cfd53a5421c51cbeb9dc37333bed80b
Kevin Savonije <kevin@23g.nl>
2025-09-07 10:58:19 +0200

    fix: only show add game button when authorized

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -5,13 +5,15 @@ import AddMatchDialog from '@/components/AddMatchDialog.vue'
 import { Button } from 'primevue'
 
 import DefaultLayout from '@/layouts/DefaultLayout.vue'
+import { useStoreAuth } from '@/stores/authStore'
 
 const showAddMatchDialog = ref(false)
+const storeAuth = useStoreAuth()
 </script>
 
 <template>
   <DefaultLayout>
-    <div class="mb-6">
+    <div class="mb-6" v-if="storeAuth.user?.id">
       <Button :label="$t('common.addMatch')" icon="pi pi-plus" @click="showAddMatchDialog = true" />
     </div>
 
