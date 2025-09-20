da5f93f762a46f057552918073fccac4df2320ed
Kevin Savonije <kevin@23g.nl>
2025-09-20 22:45:22 +0200

    feat: scorebox component

--- src/components/MatchHeader.vue
+++ src/components/MatchHeader.vue
@@ -2,43 +2,10 @@
 import { computed } from 'vue'
 import dayjs from 'dayjs'
 import type { Match } from '@/types'
-import { useStoreAuth } from '@/stores/authStore'
-import { Button, useToast } from 'primevue'
-import { useMatchStore } from '@/stores/matchStore'
-import { SEASON } from '@/constants'
-import { useI18n } from 'vue-i18n'
-
-const matchStore = useMatchStore()
+import ScoreBox from '@/components/ScoreBox.vue'
 
 const props = defineProps<{ match: Match | null }>()
-
-const authStore = useStoreAuth()
-const seasonId = SEASON
-const toast = useToast()
-const { t } = useI18n()
-
-const homeClass = computed(() => (props.match?.home ? 'flex-row' : 'flex-row-reverse text-left'))
-
-const updateGoals = async (type: 'for' | 'against', delta: 1 | -1) => {
-  if (!props.match?.id) return
-
-  const matchId = props.match.id
-  const currentGoals =
-    type === 'for' ? (props.match.result?.goalsFor ?? 0) : (props.match.result?.goalsAgainst ?? 0)
-
-  const newGoals = currentGoals + delta
-
-  await matchStore.updateMatchGoals(seasonId, matchId, type, newGoals)
-
-  if (delta > 0) {
-    toast.add({
-      severity: 'success',
-      summary: t('common.success'),
-      detail: t(`common.goalTypes.${type}`),
-      life: 3000,
-    })
-  }
-}
+const homeClass = computed(() => (props.match?.home ? 'flex-row' : 'flex-row-reverse'))
 </script>
 
 <template>
@@ -56,67 +23,12 @@ const updateGoals = async (type: 'for' | 'against', delta: 1 | -1) => {
       {{ dayjs(props.match.date.toDate()).format('D MMMM YYYY') }}
     </div>
 
-    <div
-      class="flex items-center justify-center gap-3 my-12"
-      :class="props.match?.home ? 'flex-row' : 'flex-row-reverse'"
-    >
-      <div class="flex" :class="props.match?.home ? 'flex-row' : 'flex-row-reverse'">
-        <div v-if="authStore.user?.id" class="flex justify-between gap-3 flex-col mx-6">
-          <Button
-            icon="pi pi-chevron-up"
-            severity="secondary"
-            text
-            @click="updateGoals('for', 1)"
-          />
-
-          <Button
-            icon="pi pi-chevron-down"
-            severity="secondary"
-            text
-            @click="updateGoals('for', -1)"
-          />
-        </div>
-
-        <div class="score-box">
-          <template v-if="props.match?.result?.goalsFor">
-            {{ props.match.result.goalsFor }}
-          </template>
-        </div>
-      </div>
+    <div class="flex items-center justify-center gap-3 my-12" :class="homeClass">
+      <ScoreBox :match="props.match" type="for" :reversed="props.match?.home" />
 
       <div class="text-2xl font-bold">-</div>
 
-      <div class="flex" :class="props.match?.home ? 'flex-row' : 'flex-row-reverse'">
-        <div class="score-box">
-          <template v-if="props.match?.result?.goalsAgainst">
-            {{ props.match.result.goalsAgainst }}
-          </template>
-        </div>
-
-        <div v-if="authStore.user?.id" class="flex justify-between gap-3 flex-col mx-6">
-          <Button
-            icon="pi pi-chevron-up"
-            severity="secondary"
-            text
-            @click="updateGoals('against', 1)"
-          />
-
-          <Button
-            icon="pi pi-chevron-down"
-            severity="secondary"
-            text
-            @click="updateGoals('against', -1)"
-          />
-        </div>
-      </div>
+      <ScoreBox :match="props.match" type="against" :reversed="!props.match?.home" />
     </div>
   </div>
 </template>
-
-<style scoped>
-@reference '@/styles/main.css';
-
-.score-box {
-  @apply bg-white flex items-center justify-center text-4xl sm:text-6xl font-bold size-24 sm:size-32 rounded shadow;
-}
-</style>
---
+++ src/components/ScoreBox.vue
@@ -0,0 +1,71 @@
+<script setup lang="ts">
+import { Button, useToast } from 'primevue'
+import type { Match } from '@/types'
+import { useStoreAuth } from '@/stores/authStore'
+import { useMatchStore } from '@/stores/matchStore'
+import { SEASON } from '@/constants'
+import { useI18n } from 'vue-i18n'
+
+const props = defineProps<{
+  match: Match
+  type: 'for' | 'against'
+  reversed?: boolean
+}>()
+
+const authStore = useStoreAuth()
+const matchStore = useMatchStore()
+const toast = useToast()
+const { t } = useI18n()
+
+const seasonId = SEASON
+
+const updateGoals = async (delta: 1 | -1) => {
+  if (!props.match?.id) return
+
+  const matchId = props.match.id
+  const currentGoals =
+    props.type === 'for'
+      ? (props.match.result?.goalsFor ?? 0)
+      : (props.match.result?.goalsAgainst ?? 0)
+
+  const newGoals = currentGoals + delta
+
+  await matchStore.updateMatchGoals(seasonId, matchId, props.type, newGoals)
+
+  if (delta > 0) {
+    toast.add({
+      severity: 'success',
+      summary: t('common.success'),
+      detail: t(`common.goalTypes.${props.type}`),
+      life: 3000,
+    })
+  }
+}
+</script>
+
+<template>
+  <div class="flex" :class="reversed ? 'flex-row' : 'flex-row-reverse'">
+    <div v-if="authStore.user?.id" class="flex justify-between gap-3 flex-col mx-6">
+      <Button icon="pi pi-chevron-up" severity="secondary" text @click="updateGoals(1)" />
+
+      <Button icon="pi pi-chevron-down" severity="secondary" text @click="updateGoals(-1)" />
+    </div>
+
+    <div class="score-box">
+      <template v-if="type === 'for'">
+        {{ match?.result?.goalsFor }}
+      </template>
+      <template v-else>
+        {{ match?.result?.goalsAgainst }}
+      </template>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+@reference '@/styles/main.css';
+
+.score-box {
+  @apply bg-white flex items-center justify-center text-4xl sm:text-6xl font-bold size-24 sm:size-32 rounded shadow;
+}
+</style>
